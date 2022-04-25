930781c12262c1984ded1aa5a4ae0e7f587ca6dc
Jean Raphael <jr.bordet@gmail.com>
2022-04-25 08:13:37 +0200

    wip

--- TimeInWorldFeature/Sources/TimeInWorldFeature/ContentView.swift
+++ TimeInWorldFeature/Sources/TimeInWorldFeature/ContentView.swift
@@ -6,8 +6,8 @@
 //
 
 import SwiftUI
-import TimeInWords
 import ComposableArchitecture
+import TimeInWords
 
 private let readMe = """
   This application demonstrates how to work with timers in the Composable Architecture.
@@ -17,57 +17,20 @@ private let readMe = """
   `Effect.timer` API that works with schedulers and can be tested.
   """
 
+// TimeInWorldView
 public struct ContentView: View {
     @ObservedObject var viewStore: ViewStore<TimeInWorldState, TimeInWordsAction>
-    
+
     public init(store: Store<TimeInWorldState, TimeInWordsAction>) {
         self.viewStore = ViewStore(store)
     }
-    
+
     public var body: some View {
         VStack {
             //Text(readMe)
-            
+
             Text(viewStore.timeInWords)
-            
-            ZStack {
-                Circle()
-                    .fill(
-                        AngularGradient(
-                            gradient: Gradient(
-                                colors: [
-                                    Color.blue.opacity(0.3),
-                                    .blue,
-                                    .blue,
-                                    .green,
-                                    .green,
-                                    .yellow,
-                                    .yellow,
-                                    .red,
-                                    .red,
-                                    .purple,
-                                    .purple,
-                                    Color.purple.opacity(0.3),
-                                ]
-                            ),
-                            center: .center
-                        )
-                    )
-                    .rotationEffect(Angle(degrees: -90))
-                
-                GeometryReader { proxy in
-                    Path { path in
-                        path.move(to: CGPoint(x: proxy.size.width / 2, y: proxy.size.height / 2))
-                        path.addLine(to: CGPoint(x: proxy.size.width / 2, y: 0))
-                    }
-                    .stroke(Color.black, lineWidth: 3)
-                    .rotationEffect(.degrees(Double(self.viewStore.secondsElapsed) * 360 / 60))
-                    .animation(Animation.interpolatingSpring(stiffness: 3000, damping: 40))
-                }
-            }
-            .frame(width: 280, height: 280)
-            .padding([.bottom], 16)
-            
+
             Button(action: { self.viewStore.send(.toggleTimerButtonTapped) }) {
                 HStack {
                     Text(self.viewStore.isTimerActive ? "Stop" : "Start")
@@ -77,7 +40,7 @@ public struct ContentView: View {
                 .background(self.viewStore.isTimerActive ? Color.red : .blue)
                 .cornerRadius(16)
             }
-            
+
             Spacer()
         }
         .padding()
@@ -107,3 +70,43 @@ struct ContentView_Previews: PreviewProvider {
         }
     }
 }
+
+
+//            ZStack {
+//                Circle()
+//                    .fill(
+//                        AngularGradient(
+//                            gradient: Gradient(
+//                                colors: [
+//                                    Color.blue.opacity(0.3),
+//                                    .blue,
+//                                    .blue,
+//                                    .green,
+//                                    .green,
+//                                    .yellow,
+//                                    .yellow,
+//                                    .red,
+//                                    .red,
+//                                    .purple,
+//                                    .purple,
+//                                    Color.purple.opacity(0.3),
+//                                ]
+//                            ),
+//                            center: .center
+//                        )
+//                    )
+//                    .rotationEffect(Angle(degrees: -90))
+//
+//                GeometryReader { proxy in
+//                    Path { path in
+//                        path.move(to: CGPoint(x: proxy.size.width / 2, y: proxy.size.height / 2))
+//                        path.addLine(to: CGPoint(x: proxy.size.width / 2, y: 0))
+//                    }
+//                    .stroke(Color.black, lineWidth: 3)
+//                    .rotationEffect(.degrees(Double(self.viewStore.secondsElapsed) * 360 / 60))
+//                    .animation(Animation.interpolatingSpring(stiffness: 3000, damping: 40))
+//                }
+//            }
+//            .frame(width: 280, height: 280)
+//            .padding([.bottom], 16)
+//
