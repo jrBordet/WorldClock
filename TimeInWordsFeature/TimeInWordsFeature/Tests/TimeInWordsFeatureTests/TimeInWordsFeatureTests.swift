58b5ebd0eb02e1f39747eea2e0bc10fd4a071970
Jean Raphael <jr.bordet@gmail.com>
2022-04-30 17:06:02 +0200

    Wip on Unit Tests

--- TimeInWordsFeature/TimeInWordsFeature/Tests/TimeInWordsFeatureTests/TimeInWordsFeatureTests.swift
+++ TimeInWordsFeature/TimeInWordsFeature/Tests/TimeInWordsFeatureTests/TimeInWordsFeatureTests.swift
@@ -8,7 +8,43 @@ import SwiftUI
 final class TimeInWordsFeatureTests: XCTestCase {
     let scheduler = DispatchQueue.test
     
-    func testTimeInWords() throws {
+    func testTimeInWordsDidFinishLaunchingWithOptions() throws {
+        let store = TestStore(
+            initialState: TimeInWordsState(
+                date: .eightoClock
+            ),
+            reducer: timeInWordsReducer,
+            environment: .mock(
+                mainQueue: self.scheduler.eraseToAnyScheduler(),
+                timeInWords: { h, m in
+                    Effect(value: timeInWords(h: h, m: m))
+                },
+                time12InWords: { h, m in
+                    Effect(value: time12InWords(h: h, m: m))
+                }
+            )
+        )
+        
+        store.send(.didFinishLaunchingWithOptions)
+        
+        self.scheduler.advance(by: 1)
+        
+        store.receive(.timeInWords)
+        
+        store.receive(.timeInWordsResponse(.success("eight o' clock")), {
+            $0.timeInWords = "eight o' clock"
+        })
+        
+        store.receive(.timw12InWordsResponse(.success(TimeIn12Components(hour: .eight, minutes: .zero, accessory: .o_clock))), {
+            $0.hour = .eight
+            $0.minutes = .zero
+            $0.accessory = .o_clock
+        })
+        
+        store.send(.stopTimer)
+    }
+    
+    func testTimeInWordsStart() throws {
         let store = TestStore(
             initialState: TimeInWordsState(
                 date: .eightoClock
